084337e386ab8ab08f2026c5956da42f3ba7394a
Huawei Chen <hchen@infoxchange.org>
2020-07-29 23:19:19 +1000

    1. recursive component.

---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div
+            class="item"
+            v-for="(item, index) of list"
+            :key="index"
+        >
+            <div class="item-title border-topbottom">
+                <span class="item-title-icon"><i class="icofont-ticket"></i></span>
+                {{item.title}}
+            </div>
+            <div v-if="item.children" class="children-item">
+                <detail-list :list="item.children"></detail-list>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'DetailList',
+    props: {
+      list: Array
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+    .item-title-icon
+        display inline-block
+        width .36rem
+        height .36rem
+        background-size .4rem 3rem
+    .item-title
+        line-height .8rem
+        font-size .32rem
+        padding 0 .2rem
+
+    .children-item
+        padding 0 .2rem
+</style>
