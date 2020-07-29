b834063934c8e8da1f4cea02089cedbd1ca64189
Huawei Chen <hchen@infoxchange.org>
2020-07-29 23:45:06 +1000

    1. load mock data 1. bugfix for scroll

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,14 +1,13 @@
 <template>
     <div>
         <div class="banner" @click="handleBannerClick">
-            <img class="banner-img"
-                 src="http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_600x330_1abc2773.jpg"/>
+            <img class="banner-img" :src="bannerImg"/>
             <div class="banner-info">
-                <div class="banner-title">北京野生动物园(AAAA景区)</div>
-                <div class="banner-number"><span class="icofont icofont-ui-image banner-icon"></span> 31</div>
+                <div class="banner-title">{{this.sightName}}</div>
+                <div class="banner-number"><span class="icofont icofont-ui-image banner-icon"></span> {{this.galleryImgs.length}}</div>
             </div>
         </div>
-        <common-gallery :imgs="imgs" v-show="showGallery" @close="handleGalleryClose"></common-gallery>
+        <common-gallery :imgs="galleryImgs" v-show="showGallery" @close="handleGalleryClose"></common-gallery>
     </div>
 </template>
 
@@ -17,17 +16,17 @@
 
   export default {
     name: 'DetailBanner',
+    props: {
+      sightName: String,
+      bannerImg: String,
+      galleryImgs: Array,
+    },
     components: {
       CommonGallery,
     },
     data () {
       return {
         showGallery: false,
-        imgs: [
-          'http://img1.qunarzz.com/sight/p0/1910/4c/4c37b700669d97f8a3.img.jpg_350x240_0b5ffd6f.jpg',
-          'http://img1.qunarzz.com/sight/p0/1908/99/99bb0e08aa33355ba3.img.jpg_350x240_8743a865.jpg',
-          'http://img1.qunarzz.com/sight/p0/1910/cd/cd46ea8d008759c3a3.img.jpg_350x240_252dd143.jpg',
-        ],
       }
     },
     methods: {
