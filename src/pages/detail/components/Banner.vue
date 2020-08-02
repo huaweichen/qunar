918749f2ee82eda282fcbf6b58bacbaecaf443e0
Huawei Chen <hchen@infoxchange.org>
2020-08-02 21:15:51 +1000

    minor css fix

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -62,7 +62,7 @@
             bottom 0
             line-height .6rem
             color white
-            background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+            background-image linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
 
             .banner-title
                 flex 1
