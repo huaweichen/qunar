0a69e3a815f351776364b267b8b3043dc539d766
Huawei Chen <hchen@infoxchange.org>
2020-08-05 22:27:16 +1000

    upgrade to Vue 3.0 composite API.

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,60 +1,80 @@
 <template>
     <div>
         <city-header></city-header>
-        <city-search :cities="cities"></city-search>
+        <city-search :cities="data.cities"></city-search>
         <city-list
-            :cities="cities"
-            :hotCities="hotCities"
-            :letter="letter"
+                :cities="data.cities"
+                :hotCities="data.hotCities"
+                :letter="letter"
         ></city-list>
         <city-alphabet
-            :cities="cities"
-            @change="handleLetterChange"
+                :cities="data.cities"
+                @change="handleLetterChange"
         ></city-alphabet>
     </div>
 </template>
 
 <script>
-  import axios from 'axios'
-  import CityHeader from './components/Header'
-  import CitySearch from './components/Search'
-  import CityList from './components/List'
-  import CityAlphabet from './components/Alphabet'
+    import axios from 'axios'
+    import CityHeader from './components/Header'
+    import CitySearch from './components/Search'
+    import CityList from './components/List'
+    import CityAlphabet from './components/Alphabet'
+    import {reactive, onMounted, ref} from 'vue'
 
-  export default {
-    name: 'City',
-    components: {
-      CityHeader,
-      CitySearch,
-      CityList,
-      CityAlphabet,
-    },
-    data () {
-      return {
-        cities: {},
-        hotCities: [],
-        letter: ''
-      }
-    },
-    methods: {
-      getCityInfo () {
-        axios.get('/api/city.json').then(this.handleGetCityInfoSucc)
-      },
-      handleGetCityInfoSucc (response) {
-        if (response.data.ret === true && response.data.data) {
-          const data = response.data.data
-          this.cities = data.cities
-          this.hotCities = data.hotCities
+    export default {
+        name: 'City',
+        components: {
+            CityHeader,
+            CitySearch,
+            CityList,
+            CityAlphabet,
+        },
+        setup() {
+            // Alphabetic letter related.
+            const {handleLetterChange, letter} = useLetterLogic()
+            // City related.
+            const {data} = useCityLogic()
+
+            return {
+                data,
+                letter,
+                handleLetterChange
+            }
+        },
+    }
+
+    function useLetterLogic() {
+        const letter = ref('')
+
+        function handleLetterChange(selectedLetter) {
+            letter.value = selectedLetter
         }
-      },
-      handleLetterChange (letter) {
-        this.letter = letter
-      }
-    },
-    mounted () {
-      this.getCityInfo()
-    },
-  }
+
+        return {letter, handleLetterChange}
+    }
+
+    function useCityLogic() {
+        const data = reactive({
+            cities: {},
+            hotCities: [],
+        })
+
+        async function getCityInfo() {
+            const res = await axios.get('/api/city.json');
+            const result = res.data
+            if (result.ret === true && result.data) {
+                data.cities = result.data.cities
+                data.hotCities = result.data.hotCities
+            }
+        }
+
+        onMounted(() => {
+            getCityInfo()
+        })
+
+        return {data}
+    }
 </script>
 
 <style lang="stylus" scoped>
